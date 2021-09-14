d706496334653ccced211f2329b356a84aa0716e
LucilaJaenMAC <ludelsjaen@gmail.com>
2021-09-14 18:42:49 -0500

    Se agrega el taller 2 para calcular descuento con y sin cupones

--- README.md
+++ README.md
@@ -6,3 +6,10 @@
 - 2: implementar fórmulas en JavaScript
 - 3: Crear funciones
 - 4: Integrar javaScript con HTML 
+
+## Taller # 2: Porcentajes y descuentos
+
+- 1: Definir las fórmulas
+- 2: implementar fórmulas en JavaScript
+- 3: Crear funciones
+- 4: Integrar javaScript con HTML 
---
+++ descuentos.js
@@ -0,0 +1,43 @@
+//const precioOriginal = 100;
+//const descuento = 15;
+
+function calcularPrecioConDescuento(precio, descuento, cupon){
+    let descuentoTotal = 0.0;
+    switch(cupon){
+        case "1":
+            descuentoTotal = parseFloat(descuento) + 10;
+            break;
+        case "2":
+            descuentoTotal = parseFloat(descuento) + 15;
+            break;
+        case "3":
+            descuentoTotal = parseFloat(descuento) + 25;
+            break;   
+        case "4":
+            descuentoTotal = parseFloat(descuento);
+            break; 
+        default:
+            console.log("Hubo un error");
+    }
+    const porcentajePrecioConDescuento = 100 - descuentoTotal;
+    const precioConDescuento = (precio * porcentajePrecioConDescuento) / 100;
+
+    return precioConDescuento;
+}
+
+//console.log({precioOriginal, precioConDescuento, descuento, porcentajePrecioConDescuento});
+
+function onClickButtonPriceDiscount(){
+    const inputPrice = document.getElementById("inputPrice");
+    const priceValue = inputPrice.value;
+    const inputDiscount = document.getElementById("inputDiscount");
+    const DiscountValue = inputDiscount.value;
+    const cuponDiscount = document.getElementById("cuponDiscount");
+    const cuponValue = cuponDiscount.value;
+    //console.log(cuponValue);
+    const precioConDescuento = calcularPrecioConDescuento(priceValue, DiscountValue, cuponValue);
+
+    const resultP = document.getElementById("ResultP");
+    resultP.innerText = "El precio con descuento es de: $" + precioConDescuento;
+}
+
